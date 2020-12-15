07e217386d220cce80a4c9b3ff9d5a7f7862ce04
Edward Tsai <edward.tsai.0091+github@gmail.com>
2020-12-14 21:22:40 -0800

    add day 15

--- a15/a15.py
+++ a15/a15.py
@@ -1,3 +1,25 @@
 fin = open('a15.in')
-lines = fin.readlines()
+line = fin.readline()
 fin.close()
+
+dic = {}
+last = 0
+for i,j in enumerate(line.split(',')):
+    last = int(j)
+    dic[last] = i
+
+start = len(dic.values())
+diff = 0
+for turn in range(start, 30000000):
+    if not last in dic:
+        cur = 0
+    else:
+        cur = abs(turn - 1 - dic[last])
+    dic[last] = turn - 1
+
+    last = cur
+
+
+    
+
+print(last)
