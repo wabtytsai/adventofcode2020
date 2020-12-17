c56a87438fe8603bbc5ad11ab28b93cb44ebf06d
Edward Tsai <edward.tsai.0091+github@gmail.com>
2020-12-16 22:21:46 -0800

    add day 16 and 17

--- a16/a16.py
+++ a16/a16.py
@@ -1,3 +1,82 @@
+import functools
+
 fin = open('a16.in')
-lines = fin.readlines()
+file = fin.read()
 fin.close()
+
+fields, my_ticket, tickets = file.split("\n\n")
+
+ranges = []
+dic = {}
+for field in fields.split("\n"):
+    name, values = field.split(":")
+    dic[name] = []
+    for value in values.split("or"):
+        low, high = map(int, value.strip().split("-"))
+        if high > len(ranges):
+            ranges += [1] * (high + 1)
+        for i in range(low, high + 1):
+            ranges[i] = 0
+        dic[name].append((low, high))
+        
+
+total = 0
+tickets = tickets.split("\n")[1:]
+valid_tickets = []
+for ticket in tickets:
+    values = list(map(int, ticket.split(",")))
+    valid = True
+    for value in values:
+        invalid = ranges[value] * value
+        if invalid != 0:
+            total += 1
+            valid = False
+    if valid:
+        valid_tickets.append(values)
+print(invalid)
+
+my_ticket = my_ticket.split("\n")[1]
+my_ticket = list(map(int, my_ticket.split(",")))
+
+fields = list(dic.keys())
+possibilities = [[1] * len(my_ticket) for i in range(len(fields))]
+mapping = {}
+for field in fields:
+    mapping[field] = None
+
+def validate(ranges, value):
+    for low, high in ranges:
+        if low <= value <= high: return True
+    return False
+    
+for ticket in valid_tickets:
+    for j in range(len(ticket)):
+        value = ticket[j]        
+        for i in range(len(fields)):
+            ranges = dic[fields[i]]
+            if not validate(ranges, value):
+                possibilities[j][i] = 0
+
+while not all(map(lambda x: sum(x) == 1, possibilities)):
+    for i in range(len(possibilities)):
+        possibility = possibilities[i]
+        if sum(possibility) == 1:
+            idx = possibility.index(1)
+            for j in range(len(possibilities)):
+                if i == j: continue
+                possibilities[j][idx] = 0
+mapping = {}
+for i in range(len(possibilities)):
+    idx = possibilities[i].index(1)
+    mapping[i] = fields[idx]
+
+product = 1
+for i in range(len(my_ticket)):
+    value = my_ticket[i]
+    idx = possibilities[i].index(1)
+    field = fields[idx]
+    if 'departure' in field:
+        product *= value
+print(product)
+    
+    
--- a17/a17.py
+++ a17/a17.py
@@ -1,3 +1,42 @@
 fin = open('a17.in')
 lines = fin.readlines()
 fin.close()
+lines = [line.strip() for line in lines]
+
+space = dict()
+
+for y in range(len(lines)):
+    for x in range(len(lines[0])):
+        space[(x, y, 0, 0)] = lines[y][x] == '#'
+
+def next_state(point):
+    global space
+    count = 0
+    for n in neighbours(point):
+        count += space.get(n, False)
+    if space.get(point, False):
+        return count == 2 or count == 3
+    else:
+        return count == 3
+
+def neighbours(p):
+    delta = (-1, 0, 1)
+    for dx in delta:
+        for dy in delta:
+            for dz in delta:
+                for dw in delta:
+                    if dx == dy == dz == dw == 0: continue
+                    yield p[0] + dx, p[1] + dy, p[2] + dz, p[3] + dw
+
+cycles = 6
+for i in range(cycles):
+    new_space = dict()
+    count = 0
+    for point in {point for key in space.keys() for point in neighbours(key)}:
+        state = next_state(point)
+        new_space[point] = state
+        count += state
+    space = new_space
+print(count)
+
+
